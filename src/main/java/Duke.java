b960b766466d783a17c7b41cbebb6aee82ea00d5
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-08-26 12:32:59 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,19 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you?");
+        String[] list = new String[100];
         Scanner myInput = new Scanner(System.in); // Create a scanner object
         String myString = myInput.nextLine(); // Read user input
-        while (!myString.equals("bye")) {
-            System.out.println(myString);
+        for (int i = 0; !myString.equals("bye"); i+=0) {
+            if (myString.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    System.out.println((j+1) + ". " + list[j]);
+                }
+            } else {
+                list[i] = myString;
+                System.out.println("added: " + myString);
+                i++;
+            }
             myInput = new Scanner(System.in); // Create a scanner object
             myString = myInput.nextLine(); // Read user input
         }
