5ee190923114c1874b82a1d820a979d555b223be
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-08-26 19:51:28 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,17 @@
-import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Duke {
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you?");
-        List<Task> tasks = new ArrayList<Task>();
         Scanner myInput = new Scanner(System.in); // Create a scanner object
         String myString = myInput.nextLine(); // Read user input
-        for (int i = 0; !myString.equals("bye"); i+=0) {
-            if (myString.equals("list")) {
-                System.out.println("Here are the tasks in your list: ");
-                for (int j = 0; j < i; j++) {
-                    Task task = tasks.get(j);
-                    System.out.println((j + 1) + ".[" + task.getStatusIcon() + "]" + task.description);
-                }
-            } else if (myString.length() > 4 && myString.substring(0, 4).equals("done")) {
-                int taskNumber = Integer.parseInt(myString.substring(5));
-                Task task = tasks.get(taskNumber-1);
-                task.markAsDone();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println("  ["+ "\u2713" + "] " + task.description);
-            } else {
-                Task task = new Task(myString);
-                tasks.add(task);
-                System.out.println("added: " + myString);
-                i++;
-            }
+        while (!myString.equals("bye")) {
+            System.out.println(myString);
             myInput = new Scanner(System.in); // Create a scanner object
             myString = myInput.nextLine(); // Read user input
         }
