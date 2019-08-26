aa6f6d31262ff921c32cd440be443829516b0269
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-08-26 19:58:17 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,19 @@
-import java.util.Scanner;
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Duke\n"
-                + "What can I do for you?");
-        Scanner myInput = new Scanner(System.in); // Create a scanner object
-        String myString = myInput.nextLine(); // Read user input
-        while (!myString.equals("bye")) {
-            System.out.println(myString);
-            myInput = new Scanner(System.in); // Create a scanner object
-            myString = myInput.nextLine(); // Read user input
+public static void main(String[] args) {
+    System.out.println("Hello! I'm Duke\n"
+            + "What can I do for you?");
+    String[] list = new String[100];
+    Scanner myInput = new Scanner(System.in); // Create a scanner object
+    String myString = myInput.nextLine(); // Read user input
+    for (int i = 0; !myString.equals("bye"); i+=0) {
+        if (myString.equals("list")) {
+            for (int j = 0; j < i; j++) {
+                System.out.println((j+1) + ". " + list[j]);
+            }
+        } else {
+            list[i] = myString;
+            System.out.println("added: " + myString);
+            i++;
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        myInput = new Scanner(System.in); // Create a scanner object
+        myString = myInput.nextLine(); // Read user input
     }
-}
