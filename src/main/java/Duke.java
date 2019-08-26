cd9a50140a1c712426d84715b466495185654c15
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-08-26 19:46:44 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,30 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you?");
-        String[] list = new String[100];
+        List<Task> tasks = new ArrayList<Task>();
         Scanner myInput = new Scanner(System.in); // Create a scanner object
         String myString = myInput.nextLine(); // Read user input
         for (int i = 0; !myString.equals("bye"); i+=0) {
             if (myString.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
                 for (int j = 0; j < i; j++) {
-                    System.out.println((j+1) + ". " + list[j]);
+                    Task task = tasks.get(j);
+                    System.out.println((j + 1) + ".[" + task.getStatusIcon() + "]" + task.description);
                 }
+            } else if (myString.length() > 4 && myString.substring(0, 4).equals("done")) {
+                int taskNumber = Integer.parseInt(myString.substring(5));
+                Task task = tasks.get(taskNumber-1);
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  ["+ "\u2713" + "] " + task.description);
             } else {
-                list[i] = myString;
+                Task task = new Task(myString);
+                tasks.add(task);
                 System.out.println("added: " + myString);
                 i++;
             }
