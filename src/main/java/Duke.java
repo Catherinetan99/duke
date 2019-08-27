9a91e45ef3954ac10564b78b518e15949e367885
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-08-27 12:58:03 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,23 @@
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private static final String filePath = "C:\\Users\\Catherine Tan\\IdeaProjects\\duke.txt";
+    private static int count = 0;
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n"
                 + "What can I do for you?");
-        List<Task> tasks = new ArrayList<Task>();
+        ArrayList<Task> tasks = new ArrayList<>();
         DukeException errorMessage = new DukeException();
         Scanner myInput = new Scanner(System.in); // Create a scanner object
         String myString = myInput.nextLine(); // Read user input
-        for (int i = 0; !myString.equals("bye"); i+=0) {
+        while (!myString.equals("bye")) {
             if (myString.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
-                for (int j = 0; j < i; j++) {
+                for (int j = 0; j < count; j++) {
                     Task task = tasks.get(j);
                     System.out.println((j + 1)+ "." + task.toString());
                 }
@@ -24,6 +28,7 @@ public class Duke {
                     task.markAsDone();
                     System.out.println("Nice! I've marked this task as done: ");
                     System.out.println("  " + task.toString());
+                    updateFile(tasks);
                 } catch (IndexOutOfBoundsException e) {
                     errorMessage.emptyTaskNo();
                 }
@@ -31,10 +36,11 @@ public class Duke {
                 try {
                     Todo todo = new Todo(myString.substring(5));
                     tasks.add(todo);
-                    i++;
+                    count++;
                     System.out.println("Got it. I've added this task:\n"
                             + todo.toString() + "\n"
-                            + "Now you have " + i + " tasks in the list.");
+                            + "Now you have " + count + " tasks in the list.");
+                    updateFile(tasks);
                 } catch (IndexOutOfBoundsException e) {
                     errorMessage.emptyDescription(myString.substring(0, 4));
                 }
@@ -46,10 +52,11 @@ public class Duke {
                     }
                     Deadline deadline = new Deadline(myString.substring(9, j), myString.substring(j + 4));
                     tasks.add(deadline);
-                    i++;
+                    count++;
                     System.out.println("Got it. I've added this task:\n"
                             + deadline.toString() + "\n"
-                            + "Now you have " + i + " tasks in the list.");
+                            + "Now you have " + count + " tasks in the list.");
+                    updateFile(tasks);
                 } catch (IndexOutOfBoundsException e) {
                     errorMessage.emptyDescription(myString.substring(0, 8));
                 }
@@ -61,10 +68,11 @@ public class Duke {
                     }
                     Event event = new Event(myString.substring(6, k), myString.substring(k + 4));
                     tasks.add(event);
-                    i++;
+                    count++;
                     System.out.println("Got it. I've added this task:\n"
                             + event.toString() + "\n"
-                            + "Now you have " + i + " tasks in the list.");
+                            + "Now you have " + count + " tasks in the list.");
+                    updateFile(tasks);
                 } catch (IndexOutOfBoundsException e) {
                     errorMessage.emptyDescription(myString.substring(0, 5));
                 }
@@ -75,5 +83,19 @@ public class Duke {
             myString = myInput.nextLine(); // Read user input
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void updateFile (ArrayList<Task> tasks) {
+        File f = new File(filePath);
+        try {
+            PrintWriter outputStream = new PrintWriter(f);
+            for (int i = 0; i < count; i++) {
+                Task task = tasks.get(i);
+                outputStream.println(task.toString());
+            }
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
